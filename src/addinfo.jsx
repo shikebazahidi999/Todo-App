c414ec8e709a5d1cf9779e37bc3025a1c1c5993f
shikebazahidi999 <shikebazahedi41@gmail.com>
2026-05-11 20:42:18 +0430

    all the pictures added

---
+++ src/addinfo.jsx
@@ -0,0 +1,5 @@
+export default function Addinfo(){
+    return(
+        <h1>hello from add info</h1>
+    )
+}
---
+++ src/assets/HomeWork/AllDetails.js
@@ -0,0 +1,54 @@
+export const informations = [
+     {
+        name:"Shikeba",
+        fathername:"Asadullah",
+        lastname:"Zahidi",
+        age:16,
+        class:"react.js",
+        job:"Student",
+        id:1,
+        img:'public/nature.jpg',
+        type:"Shikeba"
+    },
+    {
+        name:"Samira",
+        fathername:"Asadullah",
+        lastname:"Zahidi",
+        age:20,
+        class:"English",
+        job:"Student",
+        id:2,
+        img:'public/nature1.jpg',
+        type:"Samira"
+    },
+    {
+        name:"Fariba",
+        fathername:"Asadullah",
+        lastname:"Zahidi",
+        age:18,
+        class:"English",
+        job:"Student & Teacher",
+        img:'/public/nature2.jpg',
+        type:"Fariba"
+    },
+    {
+        name:"Amir Mohammad",
+        fathername:"Asadullah",
+        lastname:"Zahidi",
+        age:13,
+        class:"English & school : 6",
+        job:"Student",
+        img:"/public/nature3.jpg",
+        type:"Amir H"
+    },
+    {
+        name:"Amir Hussain",
+        fathername:"Asadullah",
+        lastname:"Zahidi",
+        age:11,
+        class:"English & school : 6",
+        job:"Student",
+        img:"/public/nature4.jpg",
+        type:"Amir M"
+    }
+]
