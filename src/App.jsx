f61890afc92706961b7f7d1f762d7dfda3a4ef9d
shikebazahidi999 <shikebazahedi41@gmail.com>
2026-05-11 20:41:23 +0430

    use State added to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,56 @@
-import { createContext, useContext, useState } from "react";
+// import { createContext, useContext, useState } from "react";
 
-import {  Routes,Route } from "react-router-dom";
-import Alltodo from "./Todo";
-import Done from "./done";
-import Form from "./form";
+// import {  Routes,Route } from "react-router-dom";
+// import Alltodo from "./Todo";
+// import Done from "./done";
+// import Form from "./form";
+import { createContext, useReducer } from "react";
+import Addinfo from "./addinfo";
+import Allinfo from "./Allinfo";
 
 
-export const todoContext = createContext()
+// export const todoContext = createContext()
+export const usecontextforallinfo= createContext()
 export default function App(){
-  const [value,setvalue] = useState(false);
-  const [list,setlist]= useState([]);
+//   const [value,setvalue] = useState(false);
+//   const [list,setlist]= useState([]);
+function functio(value,setvalue){
+  return(
+    value = setvalue.payload
+  )
+  
+    // if(setvalue.type==="Shikeba"){
+    //   return(
+    //     value = setvalue.payload
+    //   )
+    // }else if(setvalue.type==="Samira"){
+    //   return(
+    //     value = setvalue.payload
+    //   )
+    // }
+  
+}
+const [value,setvalue] = useReducer(functio,"")
    
  
- console.log(list)
+//  console.log(list)
   return(
-    <todoContext.Provider value={{value:value,
-    setvalue:setvalue, 
-    list: list,
-    setlist:setlist}} >
-  <Routes>
-    <Route path="/" element={<Alltodo/>}/>
-    <Route path="/Done" element={<Done/>}/>
-    <Route path="/form" element={<Form/>}/>
-  </Routes>
-  </todoContext.Provider>
+    <>
+<usecontextforallinfo.Provider value={{value:value,setvalue:setvalue}}>
+  <Allinfo/>
+</usecontextforallinfo.Provider>
+    
+    </>
+  //   <todoContext.Provider value={{value:value,
+  //   setvalue:setvalue, 
+  //   list: list,
+  //   setlist:setlist}} >
+  // <Routes>
+  //   <Route path="/" element={<Alltodo/>}/>
+  //   <Route path="/Done" element={<Done/>}/>
+  //   <Route path="/form" element={<Form/>}/>
+  // </Routes>
+  // </todoContext.Provider>
     
   )
   
